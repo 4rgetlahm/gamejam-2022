8d1f090df0827104791ff227bedd5151f0f4bbc1
Arnas Verpečinskas <arnas.verpecinskas@unity3d.com>
2022-01-29 20:13:22 +0200

    add rolling, add hitboxes

--- Assets/Scripts/Interactions/InteractionHandler.cs
+++ Assets/Scripts/Interactions/InteractionHandler.cs
@@ -16,11 +16,26 @@ public class InteractionHandler : MonoBehaviour
 
     public float MinimumDistance;
 
+    private PlayerData playerData;
+
+    private InteractableObject currentlyInterractingObject = null;
+    private CoinToss coinTosser = null;
+    private DialogHandler dialogHandler;
+
     void Start()
     {
+        coinTosser = new CoinToss();
+        dialogHandler = GameObject.FindGameObjectWithTag("DialogHandler").GetComponent<DialogHandler>();
+        playerData = GameObject.Find("Player").GetComponent<PlayerData>();
         foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Tree"))
         {
             interactables.Add(new InteractableObject(gameObject, InteractionType.WOOD));
+        }
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Stone")){
+            interactables.Add(new InteractableObject(gameObject, InteractionType.STONE));
+        }
+        foreach(GameObject gameObject in GameObject.FindGameObjectsWithTag("Enemy")){
+            interactables.Add(new InteractableObject(gameObject, InteractionType.ENEMY));
         }
     }
 
@@ -41,11 +56,56 @@ public class InteractionHandler : MonoBehaviour
 
                 if (IsNear(interactableObject))
                 {
-                    GameObject.FindGameObjectWithTag("DialogHandler").GetComponent<DialogHandler>().OpenDialog("Toss a coin to determine the result of your action?", OnGoodClick, delegate { });
-                    GameObject.Destroy(interactableObject.gameObject);
+                    currentlyInterractingObject = interactableObject;
+                    switch (interactableObject.interactionType){
+                        case InteractionType.ENEMY:
+                            dialogHandler.OpenDialog("Do you want to slay this creature? Your success rate is: " + playerData.sucessRate + "%", OnAcceptSlay, ResetInteraction);
+                            break;
+                        case InteractionType.WOOD:
+                            dialogHandler.OpenDialog("Do you want to chop the tree? Your success rate is: " + playerData.sucessRate + "%", OnAcceptChopWood, ResetInteraction);
+                            break;
+                        case InteractionType.STONE:
+                            dialogHandler.OpenDialog("Do you want to mine the stones? Your success rate is: " + playerData.sucessRate + "%", OnAcceptMineStone, ResetInteraction);
+                            break;
+                        
+                    }
                 }
             }
         }
+    }
+
+    public void OnAcceptSlay()
+    {
+        if(coinTosser.TossCoin((int) playerData.sucessRate) == Outcome.Lose){
+            playerData.Damage(10f);
+            return;
+        }
+        playerData.bones += UnityEngine.Random.Range(2, 10);
+        playerData.Heal(5f);
+        GameObject.Destroy(this.currentlyInterractingObject.gameObject);
+    }
+
+    public void OnAcceptChopWood()
+    {
+        if(coinTosser.TossCoin((int) playerData.sucessRate) == Outcome.Lose){
+            playerData.Damage(2f);
+            return;
+        }
+        playerData.wood += UnityEngine.Random.Range(5, 20);
+        GameObject.Destroy(this.currentlyInterractingObject.gameObject);
+    }
+
+    public void OnAcceptMineStone(){
+        if(coinTosser.TossCoin((int) playerData.sucessRate) == Outcome.Lose){
+            playerData.Damage(5f);
+            return;
+        }
+        playerData.stone += UnityEngine.Random.Range(3, 7);
+        GameObject.Destroy(this.currentlyInterractingObject.gameObject);
+    }
+
+    public void ResetInteraction(){
+        this.currentlyInterractingObject = null;
     }
 
 
