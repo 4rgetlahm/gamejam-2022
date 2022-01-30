f9b413dd402681b05f17575406066c5e1ac009e4
cezaris13 <pijus.cezar13@gmail.com>
2022-01-30 11:43:38 +0200

    Added sounds, on Trigger grass sounds

--- Assets/Scripts/Player/PointClickMovement.cs
+++ Assets/Scripts/Player/PointClickMovement.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
 
 public class PointClickMovement : MonoBehaviour
 {
@@ -18,8 +20,20 @@ public class PointClickMovement : MonoBehaviour
 
     public float PlayerMovementSpeed;
 
+    public AudioClip grass;
+    public AudioClip sand;
+    public AudioClip snow;
+
+    private SoundType lastSound;
+
+    private AudioSource Source;
+
+    List<GameObject> currentCollisions;
+
     void Start()
     {
+        currentCollisions = new List<GameObject>();
+        Source = GetComponent<AudioSource>();
         animator = gameObject.GetComponent<Animator>();
         animator.speed = animationSpeed;
         LastPosition = Player.GetPosition();
@@ -37,14 +51,67 @@ public class PointClickMovement : MonoBehaviour
                 navMeshAgent.speed = PlayerMovementSpeed;
                 navMeshAgent.SetDestination(hitPoint.point);
                 animator.SetBool("IsWalking", true);
+                PlayerMovementSounds();
             }
         }
         var newPosition = Player.GetPosition();
         if (LastPosition == newPosition)
+        {
             animator.SetBool("IsWalking", false);
+            Source.Stop();
+        }
         else
+        {
             animator.SetBool("IsWalking", true);
-
+            PlayerMovementSounds();
+        }
         LastPosition = newPosition;
+    }
+
+    private void PlaySound(SoundType sound)
+    {
+        if (Source.isPlaying && sound != lastSound)
+        {
+            Source.Stop();
+        }
+        if (!Source.isPlaying)
+        {
+            AudioClip clip = null;
+            switch (sound)
+            {
+                case SoundType.Grass:
+                    clip = grass;
+                    break;
+                case SoundType.Snow:
+                    clip = snow;
+                    break;
+                default:
+                    clip = sand;
+                    break;
+            }
+            Source.PlayOneShot(clip, 0.5f);
+
+        }
+        lastSound = sound;
+    }
+
+    private void PlayerMovementSounds()
+    {
+        if (currentCollisions.Select(p => p.tag).Any(p => p == "Grass"))
+            PlaySound(SoundType.Grass);
+        else if (currentCollisions.Select(p => p.tag).Any(p => p == "Snow"))
+            PlaySound(SoundType.Snow);
+        else
+            PlaySound(SoundType.Sand);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        currentCollisions.Add(col.gameObject);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        currentCollisions.Remove(col.gameObject);
     }
 }
